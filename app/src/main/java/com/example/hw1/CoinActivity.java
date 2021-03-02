ac48f373d00d40925bac34522af759bbc138799f
ARH80 <hamzei@ce.sharif.edu>
2021-03-03 00:09:33 +0330

    adding components to CoinActivity

---
+++ app/src/main/java/com/example/hw1/CoinActivity.java
@@ -0,0 +1,47 @@
+package com.example.hw1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+
+import com.github.mikephil.charting.charts.CandleStickChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+
+public class CoinActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_coin);
+/////// adding chart to activity
+        CandleStickChart candleStickChart = findViewById(R.id.candle_stick_chart);
+        candleStickChart.setHighlightPerDragEnabled(true);
+
+        candleStickChart.setDrawBorders(true);
+
+        candleStickChart.setBorderColor(getResources().getColor(R.color.lightGrey));
+
+        YAxis yAxis = candleStickChart.getAxisLeft();
+        YAxis rightAxis = candleStickChart.getAxisRight();
+        yAxis.setDrawGridLines(false);
+        rightAxis.setDrawGridLines(false);
+        candleStickChart.requestDisallowInterceptTouchEvent(true);
+
+        XAxis xAxis = candleStickChart.getXAxis();
+
+        xAxis.setDrawGridLines(false);// disable x axis grid lines
+        xAxis.setDrawLabels(false);
+        rightAxis.setTextColor(Color.WHITE);
+        yAxis.setDrawLabels(false);
+        xAxis.setGranularity(1f);
+        xAxis.setGranularityEnabled(true);
+        xAxis.setAvoidFirstLastClipping(true);
+
+        Legend l = candleStickChart.getLegend();
+        l.setEnabled(false);
+///////// TODO add set and data to candle stick chart
+    }
+}
