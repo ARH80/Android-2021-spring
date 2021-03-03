87547ba64aa6bc3c96b74cfacd89d4d55022ddf6
Saleh-Safarnejad <saleh.safarnejad@gmail.com>
2021-03-03 20:18:31 +0330

    set fields and constructor of Coin class

--- app/src/main/java/com/example/hw1/Coin.java
+++ app/src/main/java/com/example/hw1/Coin.java
@@ -1,4 +1,24 @@
 package com.example.hw1;
 
 public class Coin {
+
+    public String fullName;
+    public String symbol;
+    public String currentPrice;
+    public Object img;
+    public String percentageChange_1h;
+    public String percentageChange_24h;
+    public String percentageChange_7d;
+
+    public Coin(String fullName, String symbol, String currentPrice, Object img, String percentageChange_1h, String percentageChange_24h, String percentageChange_7d) {
+        this.fullName = fullName;
+        this.symbol = symbol;
+        this.currentPrice = currentPrice;
+        this.img = img;
+        this.percentageChange_1h = percentageChange_1h;
+        this.percentageChange_24h = percentageChange_24h;
+        this.percentageChange_7d = percentageChange_7d;
+    }
+
+
 }
