e9fbf62916793fa52c7d58a675e38a5b245748ec
Saleh-Safarnejad <saleh.safarnejad@gmail.com>
2021-03-04 17:08:47 +0330

    complete handler

--- app/src/main/java/com/example/hw1/MainActivity.java
+++ app/src/main/java/com/example/hw1/MainActivity.java
@@ -1,15 +1,20 @@
 package com.example.hw1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Message;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import java.lang.ref.WeakReference;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadPoolExecutor;
 
@@ -20,8 +25,8 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity implements CoinAdaptor.OnCoinListener {
 
     public ArrayList<Coin> coins = new ArrayList<>();
+    private UiHandler mHandler;
     private ThreadPoolExecutor executorPool;
-    private Handler mHandler;
     private Button reloadButton;
     private Button moreCoinsButton;
     private int numOfCoins = 10;
@@ -38,7 +43,8 @@ public class MainActivity extends AppCompatActivity implements CoinAdaptor.OnCoi
         coinsRecyclerView.setAdapter(coinAdaptor);
 
         this.executorPool = (ThreadPoolExecutor) Executors.newFixedThreadPool(5);
-        this.mHandler = new Handler();
+        this.mHandler = new UiHandler();
+        mHandler.setContext(this);
         this.reloadButton = (Button) findViewById(R.id.reloadBtn);
         this.moreCoinsButton = (Button) findViewById(R.id.moreCoinsBtn);
         reloadButton.setOnClickListener(new View.OnClickListener() {
@@ -53,10 +59,12 @@ public class MainActivity extends AppCompatActivity implements CoinAdaptor.OnCoi
                 addMoreCoins();
             }
         });
+
+
     }
 
     public void reload() {
-        this.executorPool.execute(new MakeApiCall(this.numOfCoins));
+            this.executorPool.execute(new MakeApiCall(numOfCoins, mHandler, this));
     }
 
 
@@ -68,5 +76,28 @@ public class MainActivity extends AppCompatActivity implements CoinAdaptor.OnCoi
     @Override
     public void onClick(int position) {
 
+    }
+
+    private static class UiHandler extends Handler{
+        private WeakReference<Context> mWeakRefContext;
+
+        public void setContext(Context context) {
+            this.mWeakRefContext = new WeakReference<Context>(context);
+        }
+
+        public WeakReference<Context> getmWeakRefContext() {
+            return mWeakRefContext;
+        }
+
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what) {
+                case -1:
+                    if (mWeakRefContext != null && mWeakRefContext.get() != null){
+
+                    }
+            }
+        }
     }
 }
--- app/src/main/java/com/example/hw1/MakeApiCall.java
+++ app/src/main/java/com/example/hw1/MakeApiCall.java
@@ -5,36 +5,56 @@ import org.jetbrains.annotations.NotNull;
 import java.io.IOException;
 import java.util.List;
 
+import android.content.Context;
+import android.nfc.Tag;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+import android.widget.Toast;
+
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import okhttp3.internal.http.HttpHeaders;
 
 public class MakeApiCall implements Runnable{
+
     private int coinsToBeShown;
     private final String url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest";
     private final String apiKey = "3675f481-d4db-4195-b319-549d5a5ce2ee";
+    private Handler uiHandler;
+    private Context context;
 
-    public MakeApiCall(int coinsToBeShown) {
+    public MakeApiCall(int coinsToBeShown, Handler uiHandler, Context context) {
         this.coinsToBeShown = coinsToBeShown;
+        this.uiHandler = uiHandler;
+        this.context = context;
     }
 
     @Override
     public void run() {
         OkHttpClient okHttpClient = new OkHttpClient();
         Request request = new Request.Builder().url(this.url)
+                .addHeader("Accept", "application/json")
                 .addHeader("X-CMC_PRO_API_KEY", this.apiKey)
                 .build();
         okHttpClient.newCall(request).enqueue(new Callback() {
             @Override
             public void onFailure(@NotNull Call call, @NotNull IOException e) {
-                //TODO
+                Log.d("error", e.getMessage());
+                Message message = new Message();
+                message.what = -1;
+                uiHandler.sendMessage(message);
             }
 
             @Override
             public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
-                //TODO
+                Message message = new Message();
+                message.obj = response;
+                message.what = 1;
+                uiHandler.sendMessage(message);
             }
         });
     }
