e09872b2ab08140615ae3921504f92bb985dd317
Saleh-Safarnejad <saleh.safarnejad@gmail.com>
2021-03-05 11:16:47 +0330

    edit Classes and handle JSON response

--- app/src/main/java/com/example/hw1/Coin.java
+++ app/src/main/java/com/example/hw1/Coin.java
@@ -2,19 +2,20 @@ package com.example.hw1;
 
 public class Coin {
 
+    public int id;
     public String fullName;
     public String symbol;
-    public String currentPrice;
-    public Object img;
-    public String percentageChange_1h;
-    public String percentageChange_24h;
-    public String percentageChange_7d;
+    public double currentPrice;
+    public double percentageChange_1h;
+    public double percentageChange_24h;
+    public double percentageChange_7d;
 
-    public Coin(String fullName, String symbol, String currentPrice, Object img, String percentageChange_1h, String percentageChange_24h, String percentageChange_7d) {
+    public Coin(int id, String fullName, String symbol, double currentPrice, double percentageChange_1h,
+                double percentageChange_24h, double percentageChange_7d) {
+        this.id = id;
         this.fullName = fullName;
         this.symbol = symbol;
         this.currentPrice = currentPrice;
-        this.img = img;
         this.percentageChange_1h = percentageChange_1h;
         this.percentageChange_24h = percentageChange_24h;
         this.percentageChange_7d = percentageChange_7d;
